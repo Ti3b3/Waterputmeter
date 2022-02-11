3554055fd9e90ff7ea4e03e9cf0ef00da1726367
Tiebeke <tiebe.declercq1@gmail.com>
2022-02-11 18:11:15 +0100

    Create WatermeterESP01s.ino

---
+++ WatermeterESP01s.ino
@@ -0,0 +1,93 @@
+/*************************************************************
+
+  This example shows how value can be pushed from Arduino to
+  the Blynk App.
+
+  NOTE:
+  BlynkTimer provides SimpleTimer functionality:
+    http://playground.arduino.cc/Code/SimpleTimer
+
+  App project setup:
+    Value Display widget attached to Virtual Pin V5
+ *************************************************************/
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID           "TMPLyTL3uAld"
+#define BLYNK_DEVICE_NAME           "Watermeter"
+#define BLYNK_AUTH_TOKEN            "3x1vbAcxIuYJV5luEr_19md-SDEIH2vV"
+
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "3x1vbAcxIuYJV5luEr_19md-SDEIH2vV";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Orange-ce3ef";
+char pass[] = "ShadowNoahBeau";
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+
+
+#define echoPin 2 // Echo Pin
+#define trigPin 0 // Trigger Pin
+
+long duration, distance; // Duration used to calculate distance
+float waterPutDepth = 200;
+int inProcent;
+
+
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V8, inProcent);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+
+   // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  inProcent = distance / waterPutDepth * 100;
+  
+}
